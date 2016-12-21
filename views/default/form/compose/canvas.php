4832daa6d7781dd635054429501009cc2cc7c228
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-12-21 11:57:16 +0100

    chore: basic views for form composing

---
+++ views/default/form/compose/canvas.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '<ul class="forms-compose-list">';
+
+echo '<li>A</li>';
+echo '<li>B</li>';
+echo '<li>C</li>';
+echo '<li>D</li>';
+
+echo '</ul>';
+
+echo ' + add page<br />';
+echo ' + add section<br />';
--- views/default/resources/forms/compose.php
+++ views/default/resources/forms/compose.php
@@ -1,1 +1,12 @@
 <?php
+
+elgg_require_js('js/forms/compose');
+
+$content = elgg_view('form/compose', $vars);
+
+$body = elgg_view_layout('one_column', [
+	'title' => $title,
+	'content' => $content,
+]);
+
+echo elgg_view_page($title, $body);
