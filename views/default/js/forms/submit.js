285dfabb886bfefdfeff1c07771b1c15b36a44c8
Jerome Bakker <jeabakker@coldtrick.com>
2017-01-18 17:16:17 +0100

    added: clientside pattern mismatch error message

--- views/default/js/forms/submit.js
+++ views/default/js/forms/submit.js
@@ -11,14 +11,22 @@ define(function(require) {
 		$('[data-conditional-field="' + name + '"][data-conditional-value="' + value + '"]').show();
 	};
 	
+	var checkValidity = function(event) {
+		// unset custom error message
+		this.setCustomValidity('');
+		
+		if (this.validity.patternMismatch) {
+			// invalid so set custom error
+			this.setCustomValidity($(this).data('customErrorMessage'));
+		}
+	};
+	
 	// sortable
 	var init = function() {
 		
 		$(document).on('change', '.forms-submit-conditional', checkConditional);
-
+		$(document).on('input', '.elgg-form-forms-submit [data-custom-error-message]', checkValidity);
 	};
 	
 	elgg.register_hook_handler('init', 'system', init);
-	
-	
 });
