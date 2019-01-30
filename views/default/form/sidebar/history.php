b383d9eed45975341c6bbf6641378f20f298ddd7
Jerome Bakker <jeabakker@coldtrick.com>
2019-01-30 11:20:28 +0100

    fixed: history sidebar uses correct system_log function

--- views/default/form/sidebar/history.php
+++ views/default/form/sidebar/history.php
@@ -1,22 +1,29 @@
 <?php
 
 $entity = elgg_extract('entity', $vars);
-if (!($entity instanceof Form)) {
+if (!$entity instanceof Form || !elgg_is_active_plugin('system_log')) {
 	return;
 }
 
-$system_log = get_system_log(false, '', Form::class, 'object', Form::SUBTYPE, 10, 0, false, 0, 0, $entity->getGUID());
+$system_log = system_log_get_log([
+	'object_class' => Form::class,
+	'object_type' => 'object',
+	'object_subtype' => Form::SUBTYPE,
+	'limit' => 10,
+	'object_id' => $entity->guid,
+]);
 if (empty($system_log)) {
 	return;
 }
 
 $result = [];
+/* @var $entry \Elgg\SystemLog\SystemLogEntry */
 foreach ($system_log as $entry) {
 	$line = [];
 	$performer = get_entity($entry->performed_by_guid);
 	if ($performer instanceof ElggUser) {
 		$line[] = elgg_view('output/url', [
-			'text' => $performer->name,
+			'text' => $performer->getDisplayName(),
 			'href' => $performer->getURL(),
 			'is_trusted' => true,
 		]);
@@ -41,7 +48,7 @@ foreach ($system_log as $entry) {
 			break;
 	}
 	
-	$line[] = elgg_format_element('span', ['class' => 'elgg-subtext'], elgg_get_friendly_time($entry->time_created));
+	$line[] = elgg_format_element('div', ['class' => 'elgg-subtext'], elgg_get_friendly_time($entry->time_created));
 	
 	$result[(int) $entry->id] = elgg_format_element('li', ['class' => 'elgg-item'], implode(' ', $line));
 }
@@ -53,6 +60,6 @@ if (empty($result)) {
 
 krsort($result);
 
-$content = elgg_format_element('ul', ['class' => 'elgg-list'], implode('', $result));
+$content = elgg_format_element('ul', ['class' => 'elgg-list'], implode(PHP_EOL, $result));
 
 echo elgg_view_module('aside', elgg_echo('forms:sidebar:history:title'), $content);
